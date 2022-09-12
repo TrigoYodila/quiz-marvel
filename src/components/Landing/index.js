e35fc2d2dfbb6fc848191ac4f369b756f1509ad8
Trigo YODILA <trigoyodila1996@gmail.com>
2022-09-12 17:40:16 +0100

    Feat : remove class after mouveout

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -16,13 +16,31 @@ const Landing = () => {
     }, 1000);
   }, []);
 
+  const setLeftImg = () => {
+    refwolverine.current.classList.add("leftImg");
+  };
+
+  const setRightImg = () => {
+    refwolverine.current.classList.add("rightImg");
+  };
+
+  
+  const clearImg = () => {
+    //vérifie si la classe rightImg existe puis supprime la
+    if(refwolverine.current.classList.contains("rightImg")){
+        refwolverine.current.classList.remove("rightImg");
+    }else if(refwolverine.current.classList.contains("leftImg")){
+        refwolverine.current.classList.remove("leftImg")
+    }
+  }
+
   //si btn = true, on affecte les btns dans display
   const displayBtn = btn && (
     <Fragment>
-      <div className="leftBox">
+      <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
         <button className="btn-welcome">Inscription</button>
       </div>
-      <div className="rightBox">
+      <div onMouseOver={setRightImg} onMouseOut={clearImg} className="rightBox">
         <button className="btn-welcome">Connexion</button>
       </div>
     </Fragment>
@@ -30,7 +48,7 @@ const Landing = () => {
 
   return (
     <main ref={refwolverine} className="welcomePage">
-     {displayBtn}
+      {displayBtn}
     </main>
   );
 };
