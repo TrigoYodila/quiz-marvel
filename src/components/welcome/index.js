08284e8aa2a00664b3c56e6c3e7571166645db2b
Trigo YODILA <trigoyodila1996@gmail.com>
2022-09-20 14:18:07 +0100

    Refactor : import logout in welcome

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Logout from "../Logout";
 
 const Welcome = () => {
   return (
     <div className="quiz-bg">
-      <div className="container">Welcome</div>
+      <div className="container">
+        <Logout />
+      </div>
     </div>
   );
 };
