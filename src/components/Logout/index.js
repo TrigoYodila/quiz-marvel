cf59350e857329e206cb494606fee80f4e5d939e
Trigo YODILA <trigoyodila1996@gmail.com>
2022-09-20 15:34:37 +0100

    Feat:deconnect user

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { useState, useEffect } from "react";
+import { FirebaseContext } from "../Firebase";
 
-const Logout  = () => {
+const Logout = () => {
+  const [checked, setChecked] = useState(false);
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(() => {
+    if (checked) {
+        //on test si le bouton deconnexion est cliqué
+      firebase.signoutUser()
+      console.log("Déconnexion");
+    }
+  }, [checked,firebase]);
+
+  const handleChange = e => {
+    setChecked(e.target.checked);
+  }
+
   return (
-    <div>
-      Logout
+    <div className="logoutContainer">
+      <label className="switch">
+        <input 
+        onChange={handleChange}
+        type="checkbox" checked={checked} />
+        <span className="slider round"></span>
+      </label>
     </div>
-  )
-}
+  );
+};
 
-export default Logout 
+export default Logout;
