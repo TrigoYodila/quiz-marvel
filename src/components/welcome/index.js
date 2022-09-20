0cde50c9d94e34bb297cbda14c69886d52c4be83
Trigo YODILA <trigoyodila1996@gmail.com>
2022-09-20 16:02:16 +0100

    Fix-bug : redirect to welcome if isn't connected

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
+import { FirebaseContext } from "../Firebase";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-  return (
+  const [userSession, setUserSession] = useState(null);
+  const firebase = useContext(FirebaseContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //verifie si l'utilisateur est connecté
+    //si l'utilisateur est connecté on aura
+    //quelque chose qui est different de null
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : navigate("/");
+    });
+
+    //fonction qui s'execute après le demontage
+    return () => {
+      listener();
+    };
+
+  }, []);
+
+  //Si la session est null on affiche un loader
+  return userSession === null ? (
+    <>
+      <div className="loader"></div>
+      <p>Loading...</p>
+    </>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
